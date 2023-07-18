60acd30c59be1407aef9a1e95ceeaad124ffca8c
njiva-niaina <niainandriarimalala@gmail.com>
2023-07-18 15:02:38 +0300

    Initial commit

---
+++ src/utils/optimum_arch.py
@@ -0,0 +1,11 @@
+class Arch:
+    delay = 1
+    corr = 100
+    # data = FunctionValues.generate_values(500, 2)
+
+    # @staticmethod
+    # def create_sequence(idx: int)
+
+    @staticmethod
+    def optimum_arch():
+        pass
